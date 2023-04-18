d8fc15d3995449159014d676f7166920889765b7
vitaxa <vitaxa93gamebox@gmail.com>
2023-04-19 00:11:02 +0300

    debug

--- yandex/main.tf
+++ yandex/main.tf
@@ -3,16 +3,3 @@ locals {
 }
 
 resource "yandex_vpc_network" "network-1" { name = "analytics" }
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  v4_cidr_blocks = ["10.1.2.0/24"]
-  name           = "analytics-subnet"
-  zone           = var.yc_region
-  network_id     = yandex_vpc_network.network-1.id
-}
-
-resource "yandex_iam_service_account" "marketdb-tf" {
-  name        = "marketdb-tf"
-  description = "service account for terraform"
-}
-
