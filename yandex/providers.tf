bac9400c2ddd3160140203e97bc9ec99f43962ce
vitaxa <vitaxa93gamebox@gmail.com>
2023-04-18 21:29:40 +0300

    Update providers.tf

--- yandex/providers.tf
+++ yandex/providers.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = "0.88.0"
     }
   }
-  required_version = ">= 0.89"
 
   backend "s3" {
     endpoint = "storage.yandexcloud.net"
