3ff55417c935de4b07bda6ab56a74e8af4cb2084
vitaxa <vitaxa93gamebox@gmail.com>
2023-10-08 13:02:27 +0300

    redis low cost cluster

--- yandex/main.tf
+++ yandex/main.tf
@@ -414,6 +414,37 @@ resource "yandex_mdb_redis_cluster" "redis_database" {
   }
 }
 
+resource "yandex_mdb_redis_cluster" "redis_database" {
+  name        = "redis_mdb"
+  environment = "PRODUCTION"
+  network_id  = yandex_vpc_network.network-1.id
+  folder_id   = var.yc_folder_id
+  tls_enabled = true
+
+  config {
+    password = var.db_password
+    version  = "7.2"
+  }
+
+  resources {
+    resource_preset_id = "ru-central1-b"
+    disk_size          = 16
+  }
+
+  host {
+    zone      = var.yc_region
+    subnet_id = yandex_vpc_subnet.redis-a.id
+    assign_public_ip = true
+  }
+
+  maintenance_window {
+    day  = "SUN"
+    hour = 2
+    type = "WEEKLY"
+  }
+}
+
+
 resource "yandex_mdb_mongodb_cluster" "mongodb_database" {
   name        = "marketdb"
   environment = "PRODUCTION"
