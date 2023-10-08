ea753ee7b5aed598ceea8edc86e22937653bc094
vitaxa <vitaxa93gamebox@gmail.com>
2023-10-08 14:26:47 +0300

    redis low cost cluster (#58)

--- yandex/providers.tf
+++ yandex/providers.tf
@@ -2,18 +2,19 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.89.0"
+      version = "0.99.1"
     }
   }
 
   backend "s3" {
-    endpoint = "storage.yandexcloud.net"
+    endpoint = "https://storage.yandexcloud.net"
     bucket   = "marketdb-tf-state"
     region   = "ru-central1"
     key      = "marketdb-tf.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
   }
 }
 
